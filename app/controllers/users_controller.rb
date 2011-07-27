b1ca71c9b3a5dc069370df19aa9df2e779ea8107
Douglas Costa <dodop3000@gmail.com>
2011-07-27 16:40:12 -0300

    CSS changes and 'sign up' page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     @title = "Sign up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #
+    else
+      @title ="Sign up"
+      render 'new'
+    end
+  end
 end
