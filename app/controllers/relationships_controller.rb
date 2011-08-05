80b592a013c1a3882ab7b516dd3d0314a510b68a
Douglas Costa <dodop3000@gmail.com>
2011-08-05 17:16:00 -0300

    cleaning code in relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,16 @@
 class RelationshipsController < ApplicationController
+  respond_to :html, :js
   before_filter :authenticate
 
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+    respond_with(@user, :location => @user)
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+    respond_with(@user, :location => @user)
   end
 end
