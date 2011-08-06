3afb5956323deb629fcc91f81ff360be473eb37d
Douglas Costa <dodop3000@gmail.com>
2011-08-06 19:28:39 -0300

    white spaces cleaned

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,7 +63,6 @@ class UsersController < ApplicationController
   end
 
   private
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
